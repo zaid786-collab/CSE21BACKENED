d0a281f17f2fdf0dcfd41a6ebc4060dac032e402
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-07-22 10:00:24 +0530

    JS WORK

---
+++ app.js
@@ -0,0 +1,27 @@
+function add(num1,num2) {
+console.log("Addition ",num1+num2);
+}
+add(2,1);
+
+// Function with return value 
+function mul(num1,num2) {
+    return num1*num2;
+}
+console.log("Multiplication : ",mul(3,7));
+
+// Arrow Function : 
+// syntax : fname=() => {}
+
+    const ml=(num1,num2,num3) => {
+        console.log("Multiplication : ",num1*num2*num3);
+    }
+
+    ml(1,2,3);
+
+    // arguments : array like object
+    // arguments do not work in arrow functions as dom will not work in backened it will work only on browser and frontend.
+
+    const ni = () => {
+        console.log(arguments);
+    }
+    ni("zaid");
