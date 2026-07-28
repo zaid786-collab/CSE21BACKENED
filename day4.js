a3a17311001b8f47410ac5f39c87bfdc31f2387a
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-07-28 13:06:04 +0530

    Async and Await

--- day4.js
+++ day4.js
@@ -1,20 +1,36 @@
 // create one promise that will display user name and password :
 // using resolve and if data wii be rejected its display error : 
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let err = true;
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let err = true;
 
-        if (err) {
-            resolve("User: CSE21, Password: 1234");
-        } else {
-            reject("ERROR... Data failed");
-        }
-    }, 2000);
-})
-.then((result) => {
-    console.log(result);
-})
-.catch((error) => {
-    console.log(error);
-});
+//         if (err) {
+//             resolve("User: CSE21, Password: 1234");
+//         } else {
+//             reject("ERROR... Data failed");
+//         }
+//     }, 2000);
+// })
+// .then((result) => {
+//     console.log(result);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
+
+
+
+// Async and Await : 
+
+console.log("this is a async/await function");
+
+async function test() {
+    console.log("1");
+    await console.log("2");
+    console.log("3");
+    console.log("4");
+}
+
+test();
+console.log("5");
