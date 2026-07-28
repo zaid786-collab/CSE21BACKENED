34cfe884757aeae5a356e71fe915219d577b0b2d
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-07-28 12:54:39 +0530

    Promises

--- day4.js
+++ day4.js
@@ -1,13 +1,20 @@
 // create one promise that will display user name and password :
 // using resolve and if data wii be rejected its display error : 
 
-new Promise(() => {
-    setTimeout( () => {
+new Promise((resolve, reject) => {
+    setTimeout(() => {
         let err = true;
-        if(!err) {
-            resolve("user : CSE21,passwword : 1234");
-        }else {
-            PromiseRejectionEvent("ERROR... data fail")
+
+        if (err) {
+            resolve("User: CSE21, Password: 1234");
+        } else {
+            reject("ERROR... Data failed");
         }
-    })
-}).then().catch()
+    }, 2000);
+})
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.log(error);
+});
